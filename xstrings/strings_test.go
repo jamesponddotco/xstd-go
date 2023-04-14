7c43653ce71bbfcd485374545da5911e1d2b4194
James Pond <james@cipher.host>
2023-04-14 19:12:39 -0300

    Add missing tests
    Signed-off-by: James Pond <james@cipher.host>

---
+++ xstrings/strings_test.go
@@ -0,0 +1,150 @@
+package xstrings_test
+
+import (
+	"testing"
+
+	"git.sr.ht/~jamesponddotco/xstd-go/xstrings"
+)
+
+func TestContainsOnly(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name   string
+		s      string
+		set    string
+		result bool
+	}{
+		{
+			name:   "EmptyString",
+			s:      "",
+			set:    xstrings.Letters,
+			result: false,
+		},
+		{
+			name:   "OnlyLetters",
+			s:      "HelloWorld",
+			set:    xstrings.Letters,
+			result: true,
+		},
+		{
+			name:   "ContainsNumbers",
+			s:      "H3ll0W0rld",
+			set:    xstrings.Letters,
+			result: false,
+		},
+		{
+			name:   "OnlyNumbers",
+			s:      "12345",
+			set:    xstrings.Numbers,
+			result: true,
+		},
+		{
+			name:   "OnlySymbols",
+			s:      "!@#$%",
+			set:    xstrings.Symbols,
+			result: true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			result := xstrings.ContainsOnly(tt.s, tt.set)
+			if result != tt.result {
+				t.Errorf("Expected %v, got %v", tt.result, result)
+			}
+		})
+	}
+}
+
+func TestRemove(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name   string
+		str    string
+		set    string
+		result string
+	}{
+		{
+			name:   "EmptyString",
+			str:    "",
+			set:    xstrings.Letters,
+			result: "",
+		},
+		{
+			name:   "RemoveAllLetters",
+			str:    "Hello123!@#",
+			set:    xstrings.Letters,
+			result: "123!@#",
+		},
+		{
+			name:   "RemoveAllNumbers",
+			str:    "H3ll0W0rld",
+			set:    xstrings.Numbers,
+			result: "HllWrld",
+		},
+		{
+			name:   "RemoveSymbols",
+			str:    "Hello@World!",
+			set:    xstrings.Symbols,
+			result: "HelloWorld",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			result := xstrings.Remove(tt.str, tt.set)
+			if result != tt.result {
+				t.Errorf("Expected %q, got %q", tt.result, result)
+			}
+		})
+	}
+}
+
+func TestJoin(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name   string
+		str    []string
+		result string
+	}{
+		{
+			name:   "EmptySlice",
+			str:    []string{},
+			result: "",
+		},
+		{
+			name:   "SingleString",
+			str:    []string{"Hello"},
+			result: "Hello",
+		},
+		{
+			name:   "MultipleStrings",
+			str:    []string{"Hello", "World", "!"},
+			result: "HelloWorld!",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			result := xstrings.Join(tt.str...)
+			if result != tt.result {
+				t.Errorf("Expected %q, got %q", tt.result, result)
+			}
+		})
+	}
+}
