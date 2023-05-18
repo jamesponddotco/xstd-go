ade97098475f4c23f97d1b82083c102c01dd439f
James Pond <james@cipher.host>
2023-05-18 18:52:56 -0300

    Add Intermediate and Modern server TLS config
    Signed-off-by: James Pond <james@cipher.host>

--- xcrypto/xtls/config.go
+++ xcrypto/xtls/config.go
@@ -26,8 +26,39 @@ func DefaultConfig() *tls.Config {
 //
 // [*tls.Config]: https://godocs.io/crypto/tls#Config
 func DefaultServerConfig() *tls.Config {
+	return ModernServerConfig()
+}
+
+// ModernServerConfig returns a [*tls.Config] using the Modern profile from the [Mozilla SSL Configuration Generator].
+//
+// [*tls.Config]: https://godocs.io/crypto/tls#Config
+// [Mozilla SSL Configuration Generator]: https://ssl-config.mozilla.org/#server=go&version=1.14.4&config=modern&guideline=5.7
+func ModernServerConfig() *tls.Config {
 	return &tls.Config{
-		MinVersion:             tls.VersionTLS13,
+		MinVersion: tls.VersionTLS13,
+		CurvePreferences: []tls.CurveID{
+			tls.X25519,
+			tls.CurveP256,
+			tls.CurveP384,
+		},
+		SessionTicketsDisabled: false,
+		ClientSessionCache:     tls.NewLRUClientSessionCache(128),
+	}
+}
+
+// IntermediateServerConfig returns a [*tls.Config] using the Intermediate profile from the [Mozilla SSL Configuration Generator].
+//
+// [*tls.Config]: https://godocs.io/crypto/tls#Config
+// [Mozilla SSL Configuration Generator]: https://ssl-config.mozilla.org/#server=go&version=1.14.4&config=intermediate&guideline=5.7
+func IntermediateServerConfig() *tls.Config {
+	return &tls.Config{
+		MinVersion:   tls.VersionTLS12,
+		CipherSuites: DefaultCipherSuites(),
+		CurvePreferences: []tls.CurveID{
+			tls.X25519,
+			tls.CurveP256,
+			tls.CurveP384,
+		},
 		SessionTicketsDisabled: false,
 		ClientSessionCache:     tls.NewLRUClientSessionCache(128),
 	}
--- xcrypto/xtls/config_test.go
+++ xcrypto/xtls/config_test.go
@@ -51,6 +51,54 @@ func TestDefaultServerConfig(t *testing.T) {
 	}
 }
 
+func TestModernServerConfig(t *testing.T) {
+	t.Parallel()
+
+	cfg := xtls.ModernServerConfig()
+
+	if cfg.MinVersion != tls.VersionTLS13 {
+		t.Errorf("Unexpected MinVersion: got %v want %v", cfg.MinVersion, tls.VersionTLS13)
+	}
+
+	if len(cfg.CurvePreferences) != 3 || cfg.CurvePreferences[0] != tls.X25519 || cfg.CurvePreferences[1] != tls.CurveP256 || cfg.CurvePreferences[2] != tls.CurveP384 {
+		t.Errorf("Unexpected CurvePreferences: got %v want [tls.X25519, tls.CurveP256, tls.CurveP384]", cfg.CurvePreferences)
+	}
+
+	if cfg.SessionTicketsDisabled {
+		t.Errorf("Unexpected SessionTicketsDisabled: got %v want false", cfg.SessionTicketsDisabled)
+	}
+
+	if cfg.ClientSessionCache == nil {
+		t.Errorf("ClientSessionCache was not initialized")
+	}
+}
+
+func TestIntermediateServerConfig(t *testing.T) {
+	t.Parallel()
+
+	cfg := xtls.IntermediateServerConfig()
+
+	if cfg.MinVersion != tls.VersionTLS12 {
+		t.Errorf("Unexpected MinVersion: got %v want %v", cfg.MinVersion, tls.VersionTLS12)
+	}
+
+	if len(cfg.CurvePreferences) != 3 || cfg.CurvePreferences[0] != tls.X25519 || cfg.CurvePreferences[1] != tls.CurveP256 || cfg.CurvePreferences[2] != tls.CurveP384 {
+		t.Errorf("Unexpected CurvePreferences: got %v want [tls.X25519, tls.CurveP256, tls.CurveP384]", cfg.CurvePreferences)
+	}
+
+	if cfg.SessionTicketsDisabled {
+		t.Errorf("Unexpected SessionTicketsDisabled: got %v want false", cfg.SessionTicketsDisabled)
+	}
+
+	if cfg.ClientSessionCache == nil {
+		t.Errorf("ClientSessionCache was not initialized")
+	}
+
+	if len(cfg.CipherSuites) == 0 {
+		t.Errorf("CipherSuites was not set")
+	}
+}
+
 func TestDefaultCipherSuites(t *testing.T) {
 	t.Parallel()
 
