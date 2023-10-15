433ab3b2efe8daab4cb0caa6fb8e0d76a444aaa6
James Pond <james@cipher.host>
2023-10-15 17:16:13 -0300

    Add function to chain middlewares
    Signed-off-by: James Pond <james@cipher.host>

--- xnet/xhttp/xmiddleware/xmiddleware.go
+++ xnet/xhttp/xmiddleware/xmiddleware.go
@@ -1,2 +1,13 @@
 // Package xmiddleware contains simple middleware functions.
 package xmiddleware
+
+import "net/http"
+
+// Chain wraps a given http.Handler with multiple middleware functions.
+func Chain(handler http.Handler, middlewares ...func(http.Handler) http.Handler) http.Handler {
+	for _, middleware := range middlewares {
+		handler = middleware(handler)
+	}
+
+	return handler
+}
---
+++ xnet/xhttp/xmiddleware/xmiddleware_test.go
@@ -0,0 +1,73 @@
+package xmiddleware_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"git.sr.ht/~jamesponddotco/xstd-go/xnet/xhttp/xmiddleware"
+)
+
+func TestChain(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name             string
+		middlewares      []func(http.Handler) http.Handler
+		requestMethod    string
+		expectStatusCode int
+	}{
+		{
+			name:             "No middlewares",
+			middlewares:      []func(http.Handler) http.Handler{},
+			requestMethod:    http.MethodGet,
+			expectStatusCode: http.StatusOK,
+		},
+		{
+			name: "Multiple middlewares",
+			middlewares: []func(http.Handler) http.Handler{
+				func(next http.Handler) http.Handler {
+					return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						w.Header().Set("X-Middleware", "1")
+						next.ServeHTTP(w, r)
+					})
+				},
+
+				func(next http.Handler) http.Handler {
+					return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						w.Header().Set("X-Middleware", "2")
+						next.ServeHTTP(w, r)
+					})
+				},
+			},
+			requestMethod:    http.MethodGet,
+			expectStatusCode: http.StatusOK,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			var (
+				w = httptest.NewRecorder()
+				r = httptest.NewRequest(tt.requestMethod, "/", http.NoBody)
+			)
+
+			var (
+				handlerToChain = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(http.StatusOK)
+				})
+				chain = xmiddleware.Chain(handlerToChain, tt.middlewares...)
+			)
+
+			chain.ServeHTTP(w, r)
+
+			if w.Code != tt.expectStatusCode {
+				t.Errorf("expected status %d, got %d", tt.expectStatusCode, w.Code)
+			}
+		})
+	}
+}
