cd16a375fd4a4ab3325df2de1d56a0baa2b7f2d3
James Pond <james@cipher.host>
2023-12-01 21:03:15 -0300

    Add common UA strings for convenience
    Signed-off-by: James Pond <james@cipher.host>

---
+++ xnet/xhttp/user_agent.go
@@ -0,0 +1,11 @@
+package xhttp
+
+// Common user agent strings. Copied from [Useragents.me] for convenience.
+//
+// [Useragents.me]: https://www.useragents.me/
+const (
+	UAEdgeWindows    = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0"
+	UAChromeWindows  = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+	UAFirefoxWindows = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0"
+	UASafariMacOS    = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Safari/605.1.1"
+)
