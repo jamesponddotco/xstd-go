c7b6ed636693906d3a257e3b8d90ebe7068ba403
James Pond <james@cipher.host>
2025-01-04 22:29:57 -0300

    Add xcontext package
    Signed-off-by: James Pond <james@cipher.host>

---
+++ xcontext/context_key.go
@@ -0,0 +1,4 @@
+package xcontext
+
+// ContextKey represents a key to be used with context.WithValue.
+type ContextKey string
---
+++ xcontext/xcontext.go
@@ -0,0 +1,4 @@
+// Package xcontext provides types to extend [the context package].
+//
+// [the context package]: https://pkg.go.dev/context
+package xcontext
