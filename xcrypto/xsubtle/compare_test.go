bb86eda9785dd5a71a6217aa07cd93faa024021b
James Pond <james@cipher.host>
2025-01-24 08:21:53 -0300

    [xsubtle] Skip expensive test in CI
    Signed-off-by: James Pond <james@cipher.host>

--- xcrypto/xsubtle/compare_test.go
+++ xcrypto/xsubtle/compare_test.go
@@ -1,6 +1,7 @@
 package xsubtle_test
 
 import (
+	"os"
 	"strings"
 	"testing"
 
@@ -14,36 +15,42 @@ func TestConstantTimeStringEqual(t *testing.T) {
 		name        string
 		giveStringA string
 		giveStringB string
+		skipCI      bool
 		want        bool
 	}{
 		{
 			name:        "equal strings",
 			giveStringA: "secret",
 			giveStringB: "secret",
+			skipCI:      false,
 			want:        true,
 		},
 		{
 			name:        "different strings same length",
 			giveStringA: "secret",
 			giveStringB: "necret",
+			skipCI:      false,
 			want:        false,
 		},
 		{
 			name:        "different lengths",
 			giveStringA: "secret",
 			giveStringB: "secrets",
+			skipCI:      false,
 			want:        false,
 		},
 		{
 			name:        "very large strings equal",
 			giveStringA: strings.Repeat("a", 1<<30),
 			giveStringB: strings.Repeat("a", 1<<30),
+			skipCI:      true,
 			want:        true,
 		},
 		{
 			name:        "very large strings different",
 			giveStringA: strings.Repeat("a", 1<<30),
 			giveStringB: strings.Repeat("b", 1<<30),
+			skipCI:      true,
 			want:        false,
 		},
 	}
@@ -51,6 +58,10 @@ func TestConstantTimeStringEqual(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
+
+			if tt.skipCI && os.Getenv("JOB_ID") != "" {
+				t.Skip()
+			}
 
 			got := xsubtle.ConstantTimeStringEqual(tt.giveStringA, tt.giveStringB)
 			if got != tt.want {
