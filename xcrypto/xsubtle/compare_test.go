cbd42dddc0bb8209c818caa5760393adfb1a71b4
James Pond <james@cipher.host>
2025-01-24 08:26:39 -0300

    [xsubtle] Skip expensive test in CI
    Signed-off-by: James Pond <james@cipher.host>

--- xcrypto/xsubtle/compare_test.go
+++ xcrypto/xsubtle/compare_test.go
@@ -11,46 +11,46 @@ import (
 func TestConstantTimeStringEqual(t *testing.T) {
 	t.Parallel()
 
+	if os.Getenv("JOB_ID") != "" {
+		t.Log("skipping expensive test in CI environment")
+
+		t.Skip()
+	}
+
 	tests := []struct {
 		name        string
 		giveStringA string
 		giveStringB string
-		skipCI      bool
 		want        bool
 	}{
 		{
 			name:        "equal strings",
 			giveStringA: "secret",
 			giveStringB: "secret",
-			skipCI:      false,
 			want:        true,
 		},
 		{
 			name:        "different strings same length",
 			giveStringA: "secret",
 			giveStringB: "necret",
-			skipCI:      false,
 			want:        false,
 		},
 		{
 			name:        "different lengths",
 			giveStringA: "secret",
 			giveStringB: "secrets",
-			skipCI:      false,
 			want:        false,
 		},
 		{
 			name:        "very large strings equal",
 			giveStringA: strings.Repeat("a", 1<<30),
 			giveStringB: strings.Repeat("a", 1<<30),
-			skipCI:      true,
 			want:        true,
 		},
 		{
 			name:        "very large strings different",
 			giveStringA: strings.Repeat("a", 1<<30),
 			giveStringB: strings.Repeat("b", 1<<30),
-			skipCI:      true,
 			want:        false,
 		},
 	}
@@ -58,10 +58,6 @@ func TestConstantTimeStringEqual(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-
-			if tt.skipCI && os.Getenv("JOB_ID") != "" {
-				t.Skip()
-			}
 
 			got := xsubtle.ConstantTimeStringEqual(tt.giveStringA, tt.giveStringB)
 			if got != tt.want {
